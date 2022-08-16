c30ece9b720e12f2c3a4c8646835c3cfdbe74e28
Uy Seng <nova@Uys-MacBook-Air.local>
2022-08-16 11:19:24 -0500

    update package.json

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/google-calendar-clone-useng"));
+app.use(express.static(__dirname + "/dist"));
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname + "/dist/google-calendar-clone-useng/index.html")
-  );
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
